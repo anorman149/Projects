8afdb73ff2668d91fa33da28db212d169b394aac
anorman149 <anormanmail@gmail.com>
2017-01-23 13:12:24 -0500

    Finished

--- Euler/Multiples of 3 and 5/src/Main.java
+++ Euler/Multiples of 3 and 5/src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] multiples = {3,5};
+        int multipleCap = 999;
+
+        System.out.println("Total from Multiples: " + addMultiples(multiples, multipleCap, 0));
     }
 
+    public static int addMultiples(int[] multiples, int multipleCap, int addition){
+
+        if(multipleCap <= 0){
+            return addition;
+        }
+
+        for(Integer multiple : multiples){
+            if(multipleCap % multiple == 0){
+                addition = addition + multipleCap;
+                break;
+            }
+        }
+
+        return addMultiples(multiples, multipleCap - 1, addition);
+    }
 }
